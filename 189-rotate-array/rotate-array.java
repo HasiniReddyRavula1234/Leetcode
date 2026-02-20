01efbf95d8f57693c330e6d49fc88b89cda3d1e2
HasiniReddyRavula1234 <175116582+HasiniReddyRavula1234@users.noreply.github.com>
2026-02-20 20:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 61.3 MB (55.28%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,76 +1,7 @@
 class Solution {
-    public void rotate(int[] arr, int k) {
-        if(arr.length == 0 || arr.length == 1 || k == 0)
-         return;
-        int n = arr.length;
-        k = k % n;
-        rotation(arr,0,n-1);
-        rotation(arr,0,k-1);
-        rotation(arr,k,n - 1);
-    }
-    public int[] rotation(int[] arr,int i,int j){
-        while(i < j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-        i++;
-        j--;
-        }
-        return arr;
-    }
-}
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //  if(k ==0 || nums.length <= 1)
-        //  return;
+    public void rotate(int[] nums, int k) {
+//  if(k ==0 || nums.length <= 1)
+//          return;
 //         int n = nums.length- 1;
 //         k = k % n;
 //         reverse(nums,0,n);
@@ -89,22 +20,22 @@ class Solution {
 //                 j--;
 //             }
         
-//         return ;
+//         return nums;
 //     }
 // }
-//  k = k % nums.length;
-//         reverse(nums,0,nums.length - 1);
-//         reverse(nums,0,k -1);
-//         reverse(nums,k,nums.length - 1);
-//     }
-//         public  void reverse(int[] arr,int start,int end){
-//             while(start < end){
-//                 int temp = arr[start] ;
-//                 arr[start] = arr[end];
-//                 arr[end] = temp;
-//                 start++;
-//                 end--;
-//             }
-//         }
-// }
+ k = k % nums.length;
+        reverse(nums,0,nums.length - 1);
+        reverse(nums,0,k -1);
+        reverse(nums,k,nums.length - 1);
+    }
+        public  void reverse(int[] arr,int start,int end){
+            while(start < end){
+                int temp = arr[start] ;
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+}
   
