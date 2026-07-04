5a4eb459bd36e4525ec8b60ae86f71baa3ee9ec4
HasiniReddyRavula1234 <175116582+HasiniReddyRavula1234@users.noreply.github.com>
2026-07-04 08:58:42 +0530

    Time: 3 ms (75.56%) | Memory: 19.5 MB (9.65%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -14,17 +14,16 @@ class Solution:
                     row[board[j][i]] = row.get(board[j][i], 0) + 1
                     if row[board[j][i]] > 1:
                         return False
-        diag = {}
         for a in range(0,9,3):
-                    for b in range(0,9,3):
-                        diag = {}
-                        for i in range(a, a + 3):
-                            for j in range(b, b + 3):
-                                if board[i][j] != '.':
-                                    diag[board[i][j]] = diag.get(board[i][j], 0) + 1
-                                    if diag[board[i][j]] > 1:
-                                        return False
-       
+            for b in range(0,9,3):
+                diag = {}
+                for i in range(a, a + 3):
+                    for j in range(b, b + 3):
+                        if board[i][j] != '.':
+                            diag[board[i][j]] = diag.get(board[i][j], 0)  + 1
+                            if diag[board[i][j]] > 1:
+                                return False
+        
                         
         return True
         
