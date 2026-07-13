d24e1963adf6a98b49c9c68f93ccd39c43e12aa6
HasiniReddyRavula1234 <175116582+HasiniReddyRavula1234@users.noreply.github.com>
2026-07-13 18:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 20 MB (60.80%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -8,6 +8,8 @@ from collections import deque
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         lst = []
+        if root is None:
+            return []
         q = deque([root])
         while q:
             qLen = len(q)
